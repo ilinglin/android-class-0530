2e15bb502859e86966ca417ae4ed8444d2fa6788
ilinglin <liniling.010@gmail.com>
2016-05-30 21:52:05 +0800

    no comment

---
+++ simpleUI/app/src/main/java/tw/com/ezstartup/simpleui/MainActivity.java
@@ -0,0 +1,18 @@
+package tw.com.ezstartup.simpleui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.layout.text);
+
+    }
+}
